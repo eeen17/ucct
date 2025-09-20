1a36c1116334ce827430d20390af3da92b29821f
Ethan Chang <ethanychang17@gmail.com>
2025-09-20 10:17:54 -0700

    working with new tasks

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import torch
 from unsloth import FastLanguageModel
 import argparse
@@ -31,55 +32,72 @@ def attachHooks(model, layers, all_layer_outputs):
 
 
 class Task(NamedTuple):
-    name: str
+    category: str
     input_header: str
-    task: str
-    br: str
     examples: str
+    task: str
     input_footer: str
 
 
-def formatTasks(model_name: str, suite: list[dict[str, str | list[str]]]) -> list[Task]:
+# {
+#     "text": [
+#         "contains fossil remains",
+#         "consists of tectonic plates",
+#         "is located at the center of Earth",
+#         "has properties of both liquids and solids"
+#     ],
+#     "label": [
+#         "A",
+#         "B",
+#         "C",
+#         "D"
+#     ]
+# }
+def formatChoices(choices: dict[str, list[str]]):
+    return "\n".join([f"{l}) {t}" for l, t in zip(choices["label"], choices["text"])])
+
+def formatTasks(model_name: str, suite: list, category_examples: dict[str, str]) -> list[Task]:
     tasks = []
     
-    match model_name:
-        case "unsloth/Qwen3-14B":
-            for t in suite:
+    for t in suite:
+        instruction = t["instruction"]
+        if "choices" in t:
+            instruction += "\n" + formatChoices(t["choices"])
+        
+        examples = category_examples[t["category"]]
+        print(examples)
+        
+        match model_name:
+            case "unsloth/gemma-3-4b-it":
                 tasks.append(
                     Task(
-                        name=str(t["name"]),
-                        input_header="<|im_start|>user\n",
-                        task=str(t["instruction"]),
-                        br = "\nExamples:\n",
-                        examples="\n".join(t["examples"]),
-                        input_footer = "<|im_end|>\n<im_start>assistant\n<think>\n\n</think>\n\n"   # disable cot
+                        category=str(t["category"]),
+                        input_header="<bos><start_of_turn>user\nAnswer the question using Answer: ... Below I've provided a few examples.\n\n",
+                        examples="Examples:\n" + examples + "\n\n",
+                        task="Question:\n" + instruction,
+                        input_footer = "<end_of_turn>\n<start_of_turn>model\n"
                     ),
                 )
-        case "unsloth/Phi-4":
-            for t in suite:
+            case "unsloth/Phi-4":
                 tasks.append(
                     Task(
-                        name=str(t["name"]),
-                        input_header="<|im_start|>user<|im_sep|>\n\n",
-                        task=str(t["instruction"]),
-                        br = "\nExamples:\n",
-                        examples="\n".join(t["examples"]),
+                        category=str(t["category"]),
+                        input_header="<|im_start|>user<|im_sep|>\nAnswer the question using Answer: ... Below I've provided a few examples.\n\n",
+                        examples="Examples:\n"+examples+"\n\n",
+                        task="Question:\n"+instruction,
                         input_footer = "\n\n<|im_end|><|im_start|>assistant<|im_sep|>\n\n"
                     ),
                 )
-        case "meta-llama/Meta-Llama-3.1-8B-Instruct":
-            for t in suite:
+            case "meta-llama/Meta-Llama-3.1-8B-Instruct":
                 tasks.append(
                     Task(
-                        name=str(t["name"]),
-                        input_header="<|start_header_id|>system<|end_header_id|>\n\nCutting Knowledge Date: December 2023\nToday Date: 26 July 2024\n\nToday Date: 26 July 2024\n\n<|eot_id|><|start_header_id|>human<|end_header_id|>\n\n",
-                        task=str(t["instruction"]),
-                        br = "<|eot_id|>\n\nExamples:\n",
-                        examples="\n".join(t["examples"]),
+                        category=str(t["category"]),
+                        input_header="<|start_header_id|>system<|end_header_id|>\n\nCutting Knowledge Date: December 2023\nToday Date: 26 July 2024\n\nToday Date: 26 July 2024\n\n<|eot_id|><|start_header_id|>human<|end_header_id|>\nAnswer the question using Answer: ... Below I've provided a few examples.\n\n",
+                        examples="Examples:\n"+examples+"\n\n",
+                        task="Question:\n"+instruction,
                         input_footer = "\n\n<|start_header_id|>assistant<|end_header_id|>\n\n"
                     ),
                 )
-
     return tasks
 
 
@@ -97,6 +115,7 @@ class Input(NamedTuple):
 def processTaskInput(task: Task, tokenizer) -> Input:
     lengths = []
     recon_tokens = []
+    print(task)
     for i, x in enumerate(task[1:]):
         section_tokens = tokenizer.encode(x)[
             0 if i == 0 else 1 :
@@ -111,8 +130,8 @@ def processTaskInput(task: Task, tokenizer) -> Input:
         lower += length
 
     return Input(
-        task_indices=indices[1],
-        examples_indices=indices[3],
+        task_indices=indices[2],
+        examples_indices=indices[1],
         tokenized=recon_tokens,
     )
 
@@ -144,19 +163,20 @@ class HiddenStatesByLayer(NamedTuple):
 
 
 def calcHiddenStates(
-    hidden_states_by_task: dict[str, HiddenStatesByLayer],
     processedTasks: list[tuple[Task, Input]],
     num_layers: int,
-    all_layer_outputs: list[torch.Tensor],
-):
-    for task, input in processedTasks:
+    task_layer_outputs: dict[int, list[torch.Tensor]],
+) -> dict[tuple[int, str], HiddenStatesByLayer]:
+    hidden_states_by_task: dict[tuple[int, str], HiddenStatesByLayer] = {}
+    
+    for task_num, (task, input) in enumerate(processedTasks):
         rho_d_by_layer: list[float] = []
         d_r_by_layer: list[float] = []
         task_vecs_by_layer: list[torch.Tensor] = []
         example_vecs_by_layer: list[torch.Tensor] = []
 
         for i in range(num_layers):
-            layer_hidden_states = all_layer_outputs[i]
+            layer_hidden_states = task_layer_outputs[task_num][i]
 
             lower, upper = input.task_indices
             task_vecs = layer_hidden_states[0, lower:upper, :]
@@ -182,29 +202,36 @@ def calcHiddenStates(
             )
             d_r_by_layer.append(d_r.item())
 
-        hidden_states_by_task[task.name] = HiddenStatesByLayer(
+        hidden_states_by_task[(task_num, task.category)] = HiddenStatesByLayer(
             rho_d_by_layer, d_r_by_layer, task_vecs_by_layer, example_vecs_by_layer
         )
+    return hidden_states_by_task
 
-def saveTasks(hidden_states_by_task: dict[str, HiddenStatesByLayer], model_name: str, output_dir: str):
+def saveTasks(hidden_states_by_task: dict[tuple[int, str], HiddenStatesByLayer], model_name: str, output_dir: str, run=None):
     rho_series = []
     d_series = []
-    for i, (task_name, results) in enumerate(hidden_states_by_task.items()):
+    for i, ((task_num, task_cat), results) in enumerate(hidden_states_by_task.items()):
+        task_name = f"{task_cat}-{task_num}"
         rho_series.append(pd.Series(results.rho_d, name=task_name))
         d_series.append(pd.Series(results.d_r, name=task_name))
     
     df_rho = pd.concat(rho_series, axis=1)
     df_d = pd.concat(d_series, axis=1)
     
+    if run is not None:
+        df_rho.to_hdf(f"{output_dir}/rho_d-{run}.h5", key='df', mode='w')
+        df_d.to_hdf(f"{output_dir}/d_r-{run}.h5", key='df', mode='w')
+        
     df_rho.to_hdf(f"{output_dir}/rho_d.h5", key='df', mode='w')
     df_d.to_hdf(f"{output_dir}/d_r.h5", key='df', mode='w')
 
 def graphByTask(
     tasks: list[Task],
-    hidden_states_by_task: dict[str, HiddenStatesByLayer],
+    hidden_states_by_task: dict[tuple[int, str], HiddenStatesByLayer],
     num_layers: int,
     model_name: str,
     output_dir: str,
+    run=None,
 ):
     all_rho_d = np.array([res.rho_d for res in hidden_states_by_task.values()])
     all_d_r = np.array([res.d_r for res in hidden_states_by_task.values()])
@@ -255,7 +282,10 @@ def graphByTask(
 
 
     plt.tight_layout()
-    plt.savefig(f"{output_dir}/agg.pdf", bbox_inches="tight")
+    if run is not None:
+        plt.savefig(f"{output_dir}/agg-{run}.pdf", bbox_inches="tight")
+    else:
+        plt.savefig(f"{output_dir}/agg.pdf", bbox_inches="tight")
     print("\nGenerating overlayed plots for all tasks...")
 
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
@@ -265,7 +295,8 @@ def graphByTask(
 
     legend_handles = []
 
-    for i, (task_name, results) in enumerate(hidden_states_by_task.items()):
+    for i, ((task_num, task_cat), results) in enumerate(hidden_states_by_task.items()):
+        task_name = f"{task_num}-{task_cat}"
         rho_d_values = results.rho_d
         d_r_values = results.d_r
 
@@ -312,10 +343,13 @@ def graphByTask(
     
     plt.tight_layout()
     plt.subplots_adjust(right=0.8)
-    plt.savefig(f"{output_dir}/overlay.pdf", bbox_inches="tight")
+    if run is not None:
+        plt.savefig(f"{output_dir}/overlay-{run}.pdf", bbox_inches="tight")
+    else:
+        plt.savefig(f"{output_dir}/overlay.pdf", bbox_inches="tight")
 
 
-def main(model_name: str, output_dir: str):
+def main(model_name: str, output_dir: str, runs=None):
     output_dir = f"{output_dir}/{model_name.replace("/", "_")}"
     os.makedirs(output_dir, exist_ok=True)
     # try:
@@ -333,60 +367,98 @@ def main(model_name: str, output_dir: str):
     )
 
     _ = FastLanguageModel.for_inference(model)
-
-    layers = model.model.layers
-        
+    
+    layers = model.model.language_model.layers if model_name == "unsloth/gemma-3-4b-it" else model.model.layers
+    
+    # manually add encode function to gemma tokenizer
+    if model_name == "unsloth/gemma-3-4b-it":
+        tokenizer.encode = lambda text: tokenizer([text])['input_ids'][0] 
+    
+            
     num_layers = len(layers)
     all_layer_outputs = [torch.tensor(0) for _ in range(num_layers)]
     attachHooks(model, layers, all_layer_outputs)
 
+    # with open('test_task_suite.json', 'r') as f:
     with open('task_suite.json', 'r') as f:
         task_suite = json.load(f)
-    tasks = formatTasks(model_name, task_suite)
+    
+    with open('examples.json', 'r') as f:
+        e = json.load(f)
+    
+    task_examples = defaultdict(str)
+    for cat, exs in e.items():
+        for ex in exs:
+            res = ex["instruction"]
+            if "choices" in ex:
+                res += "\n" + formatChoices(ex["choices"])
+            res += f"\nAnswer: {ex["answer"]}"
+            task_examples[cat] += res + "\n\n"
+    
+    
+    tasks = formatTasks(model_name, task_suite, task_examples)
     processedTasks: list[tuple[Task, Input]] = []
     for task in tasks:
         processedTasks.append((task, processTaskInput(task, tokenizer)))
-
-    task_outputs: dict[str, list[torch.Tensor]] = {}
     text_streamer = TextStreamer(tokenizer)
 
-    print(f"{'=' * 10} GENERATING {'=' * 10}")
-    for task, input in tqdm(processedTasks):
-        print(f"\n\n{'-' * 10} {task.name} {'-' * 10}\n")
-        _ = model.generate(
-            input_ids=torch.tensor([input.tokenized]).to("cuda:0"),
-            streamer=text_streamer,
-            max_new_tokens=256,
-            use_cache=True,
-        )
-        task_outputs[task.name] = all_layer_outputs.copy()
-
-    hidden_states_by_task: dict[str, HiddenStatesByLayer] = {}
-
-    calcHiddenStates(
-        hidden_states_by_task, processedTasks, num_layers, all_layer_outputs
-    )
     
-    print(f"{'=' * 10} WRITING RESULTS {'=' * 10}")
-    saveTasks(hidden_states_by_task, model_name, output_dir)
-    graphByTask(tasks, hidden_states_by_task, num_layers, model_name, output_dir)
+    task_outputs: dict[int, list[torch.Tensor]] = {}
+
+    if runs is None:
+        print(f"{'=' * 10} GENERATING {'=' * 10}")
+        for task_num, (task, input) in enumerate(tqdm(processedTasks)):
+            print(f"\n\n{'-' * 10} {task_num} {'-' * 10}\n")
+            _ = model.generate(
+                input_ids=torch.tensor([input.tokenized]).to("cuda"),
+                streamer=text_streamer,
+                max_new_tokens=256,
+                use_cache=True,
+            )
+            task_outputs[task_num] = all_layer_outputs.copy()
+
+        hidden_states_by_task = calcHiddenStates(processedTasks, num_layers, task_outputs)
+        
+        print(f"{'=' * 10} WRITING RESULTS {'=' * 10}")
+        saveTasks(hidden_states_by_task, model_name, output_dir)
+        graphByTask(tasks, hidden_states_by_task, num_layers, model_name, output_dir)
+        return
+
+    for r in range(runs):
+        print(f"{'=' * 10} GENERATING {'=' * 10}")
+        for task_num, (task, input) in enumerate(tqdm(processedTasks)):
+            print(f"\n\n{'-' * 10} {task_num} {'-' * 10}\n")
+            _ = model.generate(
+                input_ids=torch.tensor([input.tokenized]).to("cuda"),
+                streamer=text_streamer,
+                max_new_tokens=256,
+                use_cache=True,
+            )
+            task_outputs[task_num] = all_layer_outputs.copy()
+
+        hidden_states_by_task = calcHiddenStates(processedTasks, num_layers, task_outputs)
+        
+        print(f"{'=' * 10} WRITING RESULTS {'=' * 10}")
+        saveTasks(hidden_states_by_task, model_name, output_dir, run=r)
+        graphByTask(tasks, hidden_states_by_task, num_layers, model_name, output_dir, run=r)
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="VisualizeHiddenStates")
     parser.add_argument(
-        "--model_name", type=str, choices=["llama-3.1-8b-instruct", "phi-4", "gemma-3n"], default="llama-3.1-8b-instruct"
+        "--model_name", type=str, choices=["llama-3.1-8b-instruct", "phi-4", "gemma-3"], default="llama-3.1-8b-instruct"
     )
     parser.add_argument("--output_dir", type=str, default="./output")
+    parser.add_argument("--runs", type=int, default=None)
 
     args = parser.parse_args()
 
     match str(args.model_name).lower():
-        case "gemma-3n":
+        case "gemma-3":
             model_name = "unsloth/gemma-3-4b-it"
         case "phi-4":
             model_name = "unsloth/Phi-4"
         case _:
             model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     
-    main(model_name, args.output_dir)
+    main(model_name, args.output_dir, runs=args.runs)
